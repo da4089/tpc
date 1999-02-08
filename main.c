300c8c298d705a04ff57374473f5b431806b523d
phelps <phelps>
1999-02-08 16:32:23 +0000

    Updated to expect a Grammar instead of a bunch of Productions

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Lexer.h"
+#include "Grammar.h"
+
+/* Print the production rule */
+static void Frumble(void *ignored, Grammar grammar)
+{
+    Grammar_debug(grammar, stdout);
+    printf("\n");
+}
 
 /* Parse args and go */
 int main(int argc, char *argv[])
 {
-    Lexer lexer = Lexer_alloc(NULL, NULL);
+    Lexer lexer = Lexer_alloc((AcceptCallback)Frumble, NULL);
     int ch;
 
     /* Read characters from stdin and give them to the Lexer */
@@ -15,4 +23,5 @@ int main(int argc, char *argv[])
     }
 
     Lexer_acceptChar(lexer, EOF);
+    return 0;
 }
