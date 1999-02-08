d47dc06b58b025016d06808919ed6be6e6894dcb
phelps <phelps>
1999-02-08 06:56:49 +0000

    Sends stdin to the Lexer for testing

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "Lexer.h"
 
 /* Parse args and go */
 int main(int argc, char *argv[])
 {
-    printf("hello sailor\n");
+    Lexer lexer = Lexer_alloc(NULL, NULL);
+    int ch;
+
+    /* Read characters from stdin and give them to the Lexer */
+    while ((ch = fgetc(stdin)) != EOF)
+    {
+	Lexer_acceptChar(lexer, ch);
+    }
+
+    Lexer_acceptChar(lexer, EOF);
 }
