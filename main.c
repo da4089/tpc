7be0707f6f3d4e23fa53df96c8dc15ecf31d8244
phelps <phelps>
1999-12-11 17:30:31 +0000

    Modified to use parser_t instead of Lexer.  Can now specify input/ouput files on the command-line instead of always using stdin and stdout.

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include "Lexer.h"
-#include "Grammar.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include "parser.h"
+
+#define BUFFER_SIZE 4096
+
+char *input_filename = NULL;
+char *output_filename = NULL;
+int debug = 0;
 
 /* The list of long options */
 static struct option long_options[] =
 {
+    { "output", required_argument, NULL, 'o' },
     { "debug", no_argument, NULL, 'd' },
     { "version", no_argument, NULL, 'v' },
     { "help", no_argument, NULL, 'h' },
     { NULL, no_argument, NULL, '\0' }
 };
 
-
+#if 0
 /* Print the production rule */
 static void Callback(void *debug, Grammar grammar)
 {
@@ -26,11 +34,13 @@ static void Callback(void *debug, Grammar grammar)
 	Grammar_printTable(grammar, stdout);
     }
 }
+#endif
 
-/* Prints out the Usage message */
-static void Usage(int argc, char *argv[])
+/* Prints out the usage message */
+static void usage(int argc, char *argv[])
 {
-    fprintf(stderr, "usage: %s [OPTION]...\n", argv[0]);
+    fprintf(stderr, "usage: %s [OPTION]... [FILE]\n", argv[0]);
+    fprintf(stderr, "  -o file,     --output=file\n");
     fprintf(stderr, "  -d,          --debug\n");
     fprintf(stderr, "  -v,          --version\n");
     fprintf(stderr, "  -h,          --help\n");
@@ -40,16 +50,22 @@ static void Usage(int argc, char *argv[])
 /* Parse args and go */
 int main(int argc, char *argv[])
 {
-    Lexer lexer;
+    parser_t parser;
     int choice;
-    int debug = 0;
-    int ch;
+    int fd;
 
     /* Read options from the command line */
-    while ((choice = getopt_long(argc, argv, "dvh", long_options, NULL)) > 0)
+    while ((choice = getopt_long(argc, argv, "o:dvh", long_options, NULL)) > 0)
     {
 	switch (choice)
 	{
+	    /* --output or -o */
+	    case 'o':
+	    {
+		output_filename = optarg;
+		break;
+	    }
+
 	    /* --debug or -d */
 	    case 'd':
 	    {
@@ -67,27 +83,71 @@ int main(int argc, char *argv[])
 	    /* --help or -h */
 	    case 'h':
 	    {
-		Usage(argc, argv);
+		usage(argc, argv);
 		exit(0);
 	    }
 
 	    /* bogus option */
 	    default:
 	    {
-		Usage(argc, argv);
+		usage(argc, argv);
 		exit(1);
 	    }
 	}
     }
-    
-    /* Create the lexer */
-    lexer = Lexer_alloc((AcceptCallback)Callback, (void *)debug);
-    /* Read characters from stdin and give them to the Lexer */
-    while ((ch = fgetc(stdin)) != EOF)
+
+    /* Look for an input file name */
+    if (optind < argc)
     {
-	Lexer_acceptChar(lexer, ch);
+	input_filename = argv[optind++];
     }
 
-    Lexer_acceptChar(lexer, EOF);
-    return 0;
+    /* Make sure we don't have any extra args */
+    if (optind < argc)
+    {
+	usage(argc, argv);
+	exit(1);
+    }
+    
+    /* Create the parser */
+    if ((parser = parser_alloc(NULL, NULL)) == NULL)
+    {
+	perror("parser_alloc(): failed");
+	exit(1);
+    }
+
+    /* Open up the input file */
+    if (input_filename != NULL)
+    {
+	if ((fd = open(input_filename, O_RDONLY)) < 0)
+	{
+	    perror("open(): failed");
+	    exit(1);
+	}
+    }
+    else
+    {
+	fd = STDIN_FILENO;
+    }
+
+    /* Read characters from stdin and give them to the Lexer */
+    while (1)
+    {
+	char buffer[BUFFER_SIZE];
+	ssize_t length;
+
+	if ((length = read(fd, buffer, 2048)) < 0)
+	{
+	    perror("read(): failed");
+	    abort();
+	}
+
+	parser_parse(parser, buffer, length);
+
+	if (length == 0)
+	{
+	    close(fd);
+	    return 0;
+	}
+    }
 }
