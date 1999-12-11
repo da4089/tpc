861178af7ed955d3b234bf825a37d6a0430a79af
phelps <phelps>
1999-12-11 16:57:39 +0000

    Oops!  Need this to be the header file for grammar.c...

--- grammar.h
+++ grammar.h
@@ -1,113 +1,53 @@
-#define IS_ERROR(action) ((action) == 0)
-#define IS_ACCEPT(action) ((action) == 28)
-#define IS_REDUCE(action) (0 < (action) && (action) < 11)
-#define IS_SHIFT(action) (11 <= (action) && (action) < 28)
-#define REDUCTION(action) (action)
-#define REDUCE_GOTO(state, production) \
-    (goto_table[state][production -> nonterm_type])
-#define SHIFT_GOTO(action) ((action) - 11)
+/***************************************************************
 
-typedef enum
-{
-    TT_EOF = 0,
-    TT_DERIVES,
-    TT_ID,
-    TT_STRING,
-    TT_LBRACKET,
-    TT_RBRACKET,
-} terminal_t;
+  Copyright (C) DSTC Pty Ltd (ACN 052 372 577) 1995.
+  Unpublished work.  All Rights Reserved.
 
-struct production
-{
-    void *(*function)();
-    int nonterm_type;
-    int count;
-};
+  The software contained on this media is the property of the
+  DSTC Pty Ltd.  Use of this software is strictly in accordance
+  with the license agreement in the accompanying LICENSE.DOC
+  file.  If your distribution of this software does not contain
+  a LICENSE.DOC file then you have no rights to use this
+  software in any manner and should contact DSTC at the address
+  below to determine an appropriate licensing arrangement.
 
-static struct production productions[11] =
-{
-    /* 0: <grammar> ::= <production-list> */
-    { accept_grammar, 0, 1 },
+     DSTC Pty Ltd
+     Level 7, Gehrmann Labs
+     University of Queensland
+     St Lucia, 4072
+     Australia
+     Tel: +61 7 3365 4310
+     Fax: +61 7 3365 4311
+     Email: enquiries@dstc.edu.au
 
-    /* 1: <production-list> ::= <production-list> <production> */
-    { extend_production_list, 1, 2 },
+  This software is being provided "AS IS" without warranty of
+  any kind.  In no event shall DSTC Pty Ltd be liable for
+  damage of any kind arising out of or in connection with
+  the use or performance of this software.
 
-    /* 2: <production-list> ::= <production> */
-    { make_production_list, 1, 1 },
+****************************************************************/
 
-    /* 3: <production> ::= <nonterminal> DERIVES <exp-list> <function> */
-    { make_production, 2, 4 },
+#ifndef GRAMMAR_H
+#define GRAMMAR_H
 
-    /* 4: <exp-list> ::= <exp-list> <nonterminal> */
-    { add_component, 4, 2 },
+#ifndef lint
+static const char cvs_GRAMMAR_H[] = "$Id: grammar.h,v 1.10 1999/12/11 16:57:39 phelps Exp $";
+#endif /* lint */
 
-    /* 5: <exp-list> ::= <exp-list> <terminal> */
-    { add_component, 4, 2 },
+/* The grammar type */
+typedef struct grammar *grammar_t;
 
-    /* 6: <exp-list> ::= <nonterminal> */
-    { make_exp_list, 4, 1 },
 
-    /* 7: <exp-list> ::= <terminal> */
-    { make_exp_list, 4, 1 },
+/* Allocates and initializes a new nonterminal grammar_t */
+grammar_t grammar_alloc();
 
-    /* 8: <nonterminal> ::= ID */
-    { make_nonterminal, 3, 1 },
+/* Releases the resources consumed by the receiver */
+void grammar_free(grammar_t self);
 
-    /* 9: <terminal> ::= STRING */
-    { make_terminal, 6, 1 },
+/* Adds another production to the grammar */
+void grammar_add_production(grammar_t self, production_t production);
 
-    /* 10: <function> ::= LBRACKET ID RBRACKET */
-    { make_function, 5, 3 }
-};
+/* Pretty-prints the receiver */
+void grammar_print(grammar_t self, FILE *out);
 
-#define ERR 0
-#define ACC 28
-#define R(x) (x)
-#define S(x) (x + 11)
-
-static unsigned char sr_table[17][6] =
-{
-    { ERR, ERR, S(4), ERR, ERR, ERR },
-    { ACC, ERR, S(4), ERR, ERR, ERR },
-    { R(2), ERR, R(2), ERR, ERR, ERR },
-    { ERR, S(6), ERR, ERR, ERR, ERR },
-    { ERR, R(8), R(8), R(8), R(8), ERR },
-    { R(1), ERR, R(1), ERR, ERR, ERR },
-    { ERR, ERR, S(4), S(10), ERR, ERR },
-    { ERR, ERR, R(6), R(6), R(6), ERR },
-    { ERR, ERR, S(4), S(10), S(14), ERR },
-    { ERR, ERR, R(7), R(7), R(7), ERR },
-    { ERR, ERR, R(9), R(9), R(9), ERR },
-    { ERR, ERR, R(4), R(4), R(4), ERR },
-    { R(3), ERR, R(3), ERR, ERR, ERR },
-    { ERR, ERR, R(5), R(5), R(5), ERR },
-    { ERR, ERR, S(15), ERR, ERR, ERR },
-    { ERR, ERR, ERR, ERR, ERR, S(16) },
-    { R(10), ERR, R(10), ERR, ERR, ERR },
-};
-
-#undef ERR
-#undef R
-#undef S
-
-static unsigned char goto_table[17][7] = 
-{
-    { 0, 1, 2, 3, 0, 0, 0 },
-    { 0, 0, 5, 3, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 7, 8, 0, 9 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 11, 0, 12, 13 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0 },
-};
-
+#endif /* GRAMMAR_H */
