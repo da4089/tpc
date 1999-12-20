ab3010ae9e9fef1c53f7b02d5baa29d564e4a9b9
phelps <phelps>
1999-12-20 15:53:08 +0000

    Added the `--quiet' command-line option

--- main.c
+++ main.c
@@ -1,3 +1,33 @@
+/***************************************************************
+
+  Copyright (C) DSTC Pty Ltd (ACN 052 372 577) 1995.
+  Unpublished work.  All Rights Reserved.
+
+  The software contained on this media is the property of the
+  DSTC Pty Ltd.  Use of this software is strictly in accordance
+  with the license agreement in the accompanying LICENSE.DOC
+  file.  If your distribution of this software does not contain
+  a LICENSE.DOC file then you have no rights to use this
+  software in any manner and should contact DSTC at the address
+  below to determine an appropriate licensing arrangement.
+
+     DSTC Pty Ltd
+     Level 7, Gehrmann Labs
+     University of Queensland
+     St Lucia, 4072
+     Australia
+     Tel: +61 7 3365 4310
+     Fax: +61 7 3365 4311
+     Email: enquiries@dstc.edu.au
+
+  This software is being provided "AS IS" without warranty of
+  any kind.  In no event shall DSTC Pty Ltd be liable for
+  damage of any kind arising out of or in connection with
+  the use or performance of this software.
+
+****************************************************************/
+
+#include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -61,6 +91,7 @@ static void usage(int argc, char *argv[])
     fprintf(stderr, "usage: %s [OPTION]... [FILE]\n", argv[0]);
     fprintf(stderr, "  -o file,     --output=file\n");
     fprintf(stderr, "  -d,          --debug\n");
+    fprintf(stderr, "  -q,          --quiet\n");
     fprintf(stderr, "  -v,          --version\n");
     fprintf(stderr, "  -h,          --help\n");
 }
@@ -74,7 +105,7 @@ int main(int argc, char *argv[])
     int fd;
 
     /* Read options from the command line */
-    while ((choice = getopt_long(argc, argv, "o:dvh", long_options, NULL)) > 0)
+    while ((choice = getopt_long(argc, argv, "o:dqvh", long_options, NULL)) > 0)
     {
 	switch (choice)
 	{
@@ -89,6 +120,13 @@ int main(int argc, char *argv[])
 	    case 'd':
 	    {
 		debug = 1;
+		break;
+	    }
+
+	    /* --quiet or -q */
+	    case 'q':
+	    {
+		fclose(stderr);
 		break;
 	    }
 
